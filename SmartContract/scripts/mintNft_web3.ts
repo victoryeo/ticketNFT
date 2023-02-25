58420d26e50caf6725164ee308f360da4f17311b
yeosiowvic <victoryeo1974@gmail.com>
2023-02-25 19:21:56 +0800

    update TICKET_NFT contract address

--- SmartContract/scripts/mintNft_web3.ts
+++ SmartContract/scripts/mintNft_web3.ts
@@ -27,10 +27,10 @@ let nftInst = new web3.eth.Contract(
   nftContract.abi, NFT_CONTRACT_ADDRESS
 )
 
-const NFT_SUPPLY: number = 3
-const NFT_PER_BATCH: number = 100;
+const NFT_SUPPLY: number = 1000
+const NFT_PER_BATCH: number = 100
 
-const multicall = new Multicall({ web3Instance: web3, tryAggregate: true });
+const multicall = new Multicall({ web3Instance: web3, tryAggregate: true })
 
 async function mintNFT() {
   let accounts: string[] = await web3.eth.getAccounts()
@@ -46,14 +46,14 @@ async function mintNFT() {
 
     // mint NFT to organiser 
     // accounts[0] is organiser wallet
-    const callsData: CallContext[] = [];
+    const callsData: CallContext[] = []
     for (let i = 1; i <= NFT_SUPPLY; i++) {
       // multicall of smart contracts, for view methods only
       /*callsData.push({
         reference: String(i),
         methodName: 'ownerOf',
         methodParameters: [i],
-      });
+      })
       if (
         i % NFT_PER_BATCH === 0 ||
         (i === NFT_SUPPLY && callsData.length > 0)
@@ -64,7 +64,7 @@ async function mintNFT() {
           abi: nftContract.abi,
           //from 0 to callsData.length of methods per multicall
           calls: callsData.splice(0, callsData.length),
-        });
+        })
         console.log(JSON.stringify(results))
         //console.log(callsData.length)
         //console.log(callsData.splice(0, callsData.length))
@@ -72,12 +72,12 @@ async function mintNFT() {
 
       // original way of minting NFT
       const data0 = await nftInst.methods.mintNFT(accounts[0]).send({from: accounts[0]})
-      console.log(data0);
+      console.log(data0)
     }
 
     // show NFT number
     const data1 = await nftInst.methods.totalSupply().call()
-    console.log(data1);    
+    console.log(data1)
 
   } catch (err) {
     console.log(err)
