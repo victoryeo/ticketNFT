9f35fca6c7552b1d1608a379ad2d1a893c13322f
yeosiowvic <victoryeo1974@gmail.com>
2023-02-27 14:57:05 +0800

    show  NFT tokenID owned by account

--- ReactJS/components/NFT/NFT.tsx
+++ ReactJS/components/NFT/NFT.tsx
@@ -9,12 +9,21 @@ import {
   TableCell,
   Button,
 } from "@mui/material";
+import {
+  Multicall,
+  ContractCallResults,
+  ContractCallContext,
+} from "ethereum-multicall";
+import { CallContext } from "ethereum-multicall/dist/esm/models";
 import { useSelector } from "react-redux";
 import contracts from "../../config/constants/contracts";
+import { tn_abi } from '../../config/abi/TicketNFT'
 import styles from "./NFT.module.css";
 import { selectSigner } from "../../redux/selectors"
 import { getTNContract } from "../../utils/web3Utils";
 import { selectUserAddress } from "../../redux/selectors/user";
+
+const NFT_PER_BATCH = 100
 
 const imgsrc = "https://gateway.pinata.cloud/ipfs/QmcxJNpGFmAfxVwh56ik8v7DFHxRHCm6m1QfZGt3wKsWuW"
 const nft_con_address = contracts.TICKET_NFT[5]
@@ -23,9 +32,14 @@ let account: string;
 
 export default function NFT() {
   const [NFTTotalSupply, setNFTTotalSupply] = useState<number>(0);
+  const [NFTOwned, setNFTOwned] = useState<number[]>([]);
   const signer = useSelector(selectSigner);
   contractTN = getTNContract(signer);
   account = useSelector(selectUserAddress);
+
+  const provider = ethers.getDefaultProvider(process.env.REACT_APP_WEB3_PROVIDER_HTTPS)
+  console.log(provider)
+  const multicall = new Multicall({ ethersProvider: provider, tryAggregate: true })
 
   useEffect(() => {
     const fetchContractAsset = async() => {
@@ -39,6 +53,64 @@ export default function NFT() {
     }
     fetchContractAsset()
   }, []);
+
+  useEffect(() => {
+    const fetchNftAsset = async() => {
+      const callsData: CallContext[] = []
+      try {
+        const owned: number[] = [];
+        for (let i = 1; i <= NFTTotalSupply; i++) {
+          // multicall of smart contracts, for view methods only
+          // multicall does not support set funtions
+          callsData.push({
+            reference: String(i),
+            methodName: 'ownerOf',
+            methodParameters: [i],
+          })
+          if (
+            i % NFT_PER_BATCH === 0 ||
+            (i === NFTTotalSupply && callsData.length > 0)
+          )  {
+            owned.push (
+              ...Object.values(
+                (await multicall.call({
+                  reference: 'info',
+                  contractAddress: contracts.TICKET_NFT[5],
+                  abi: tn_abi,
+                  //from 0 to callsData.length of methods per multicall
+                  calls: callsData.splice(0, callsData.length),
+                })
+              ).results.info.callsReturnContext
+              ).map(function(r, index) {
+                if (r.returnValues[0].toLowerCase() == account) {
+                  console.log("You own NFT tokenID ",index+1)
+                  return index+1
+                }
+              })
+            )
+            console.log(owned)
+          }
+        }
+        const newState = owned.map((item) => {
+          if (item !== undefined) {
+            return item
+          }
+        })
+        console.log(newState)
+        setNFTOwned(newState)
+      } catch (e) {
+        console.log("fetchNftAsset", e)
+      }
+    }
+    setNFTOwned([]) //reset the state at each invocation
+    fetchNftAsset()
+  }, [NFTTotalSupply])
+
+  const nftItems = NFTOwned.map((item, index)=> 
+    <TableCell style={{width: '1%'}} key={index}>
+      {item}
+    </TableCell>
+  )
 
   return(
     <div className={styles.container}>
@@ -69,6 +141,13 @@ export default function NFT() {
               <TableCell style={{width: '15%'}}>
                 {NFTTotalSupply}
               </TableCell>  
+            </TableRow>
+            <TableRow>
+              <TableCell style={{width: '30%'}}>
+                NFT owned by you <br/>
+                (sorted by TokenID):
+              </TableCell>
+              {nftItems}
             </TableRow>
           </TableBody>
         </Table>
