4f98359a4638594d37fb10c01a244b0b40541529
yeosiowvic <victoryeo1974@gmail.com>
2023-03-01 21:14:46 +0800

    add order processing skeleton code

--- OrderBackend/models/order.go
+++ OrderBackend/models/order.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type Order struct {
 	ID      uint   `json:"id" gorm:"primary_key"`
 	Type    string `json:"type"`
@@ -11,4 +13,35 @@ type CreateOrderInput struct {
 	Type    string `json:"type" binding:"required"`
 	TokenID string `json:"tokenID" binding:"required"`
 	Price   string `json:"price" binding:"required"`
+}
+
+// OrderBook type
+type OrderBook struct {
+	BuyOrders  []Order
+	SellOrders []Order
+}
+
+// Process an order and return the order matched(if any)
+func Process(order Order) Order {
+	var matchOrder Order
+	if order.Type == "buy" {
+		fmt.Print("process buy order\n")
+		matchOrder = processLimitBuy(order)
+	} else if order.Type == "sell" {
+		fmt.Print("process sell order\n")
+		matchOrder = processLimitSell(order)
+	} else {
+		fmt.Print("wrong order type\n")
+	}
+	return matchOrder
+}
+
+func processLimitBuy(order Order) Order {
+	//TODO: add processing logic
+	return order
+}
+
+func processLimitSell(order Order) Order {
+	//TODO: add processing logic
+	return order
 }
