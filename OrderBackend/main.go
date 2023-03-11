80ae0c2e92dc000515ec815d47a7851be54e32ab
yeosiowvic <victoryeo1974@gmail.com>
2023-03-11 09:42:38 +0800

    add golang testcode

--- OrderBackend/main.go
+++ OrderBackend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -40,17 +42,53 @@ func CORSMiddleware() gin.HandlerFunc {
 	}
 }
 
+// 0 1 1 2 3 5 8
+func fibonacci(n uint) uint {
+	var result uint
+
+	if n > 1 {
+		result = fibonacci(n-1) + fibonacci(n-2)
+	} else {
+		result = 1
+	}
+
+	return result
+}
+
+type ServerConn struct {
+	ID  string
+	age uint
+}
+
 func main() {
 	fmt.Print("Order Backend ", "started.\n")
 	models.InitOrder()
 
-	// an golang uint variable cannot go to negative value
+	// golang uint variable cannot go to negative value
 	var ans uint = 0
 	var topNum uint = 5
 	for i := topNum; i >= 1; i-- {
 		ans += i
 	}
-	fmt.Print("ans\n")
+	fmt.Print("ans ", ans, "\n")
+
+	// golang array
+	buildTags := []string{"cli_no_docs", "cli_docs"}
+	buildTags = append(buildTags, "osusergo", "netgo")
+	if len(buildTags) > 0 {
+		log.Printf("buildTags > 0")
+		log.Printf("%v", buildTags)
+	}
+
+	// golang atomic pointer
+	p := atomic.Pointer[ServerConn]{}
+	s := ServerConn{ID: "first_conn", age: 5}
+	p.Store(&s)
+	log.Printf("%v", p)
+	fmt.Println(p.Load())
+
+	// fibanacci
+	fmt.Println("fibanocci", fibonacci(4))
 
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
